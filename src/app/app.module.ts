217c664e493ce7c3ddee4eb64de765487b53036f
Roope Sakari Kivioja <roope.kivioja@gmail.com>
2018-02-24 20:08:15 +0200

    Add localization module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
+//translation import
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { MainPage } from '../pages/main/main';
@@ -15,6 +20,10 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { Ng2EmojiModule } from 'ng2-emoji';
 import { PageDataProvider } from '../providers/page-data/page-data';
 
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
 @NgModule({
   declarations: [
     MyApp,
@@ -27,7 +36,15 @@ import { PageDataProvider } from '../providers/page-data/page-data';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
-    Ng2EmojiModule
+    Ng2EmojiModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (createTranslateLoader),
+        deps: [HttpClient]
+      }
+    })
   ],
   bootstrap: [IonicApp],
   entryComponents: [
